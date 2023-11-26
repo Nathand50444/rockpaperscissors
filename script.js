f99ad19d4cf377e28e586f8f8b6ca01594cd8dc0
NathanD <nathand50444@gmail.com>
2023-11-26 19:49:03 +0000

    Rock, Paper, Scissors [build] Created the game.
    All required elements from the assignment included. The game keeps score of the player and computer, first to five wins is the winner.

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+    let computerScore = 0;
+    let playerScore = 0;
+    let playerSelection;
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    document.getElementById("rock").addEventListener("click", function() {
+      makeSelection('rock');
+    });
+
+    document.getElementById("paper").addEventListener("click", function() {
+      makeSelection('paper');
+    });
+
+    document.getElementById("scissors").addEventListener("click", function() {
+      makeSelection('scissors');
+    });
+    
+    document.getElementById("reset").addEventListener("click", function() {
+        resetGame();
+    });
+  });
+
+function makeSelection(selection) {
+    playerSelection = selection;
+    playGame();
+}
+
 function computerPlay() {
     const randomIndex = Math.floor(Math.random()*3);
     const computerSelection = ['Rock', 'Paper', 'Scissors'];
@@ -7,19 +35,54 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+
+    let result;
+
     if (playerSelection === computerSelection) {
-        return `It's a draw! You both chose ${playerSelection}`;
+        result = `It's a draw! You both chose ${playerSelection}.`;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "You win! Rock beats Scissors!";
+        result = "You win! Rock beats Scissors!";
+        playerScore++;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return "You win! Scissors beats Paper!";
+        result = "You win! Scissors beats Paper!";
+        playerScore++;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return "You win! Paper beats Rock!";
+        result = "You win! Paper beats Rock!";
+        playerScore++;
     } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        result = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        computerScore++;
     }
+
+    document.getElementById('result').innerText = `Player chose ${playerSelection}. Computer chose ${computerSelection}. ${result}`;
+    document.getElementById('scoreboard').innerText = `Score: Player ${playerScore} - Computer ${computerScore}`;
+
+    if (playerScore === 5 || computerScore === 5) {
+        endGame();
+    }
+
+    console.log(result);
+};
+
+function playGame() {
+const computerSelection = computerPlay();
+playRound(playerSelection, computerSelection)
+};
+
+function endGame() {
+    let endMessage;
+    if (playerScore === 5) {
+        endMessage = "Congratulations! You've won the game!";
+    } else {
+        endMessage = "Game over! Computer wins!";
+    }
+
+    document.getElementById('result').innerText = endMessage;
 }
 
-const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection))
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById('scoreboard').innerText = 'Score: Player 0 - Computer 0';
+    document.getElementById('result').innerText = 'Choose Rock, Paper, or Scissors';
+}
